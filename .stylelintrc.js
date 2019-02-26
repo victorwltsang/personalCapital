faeab85bd419741c306fa5a80edd3dbfd2555c70
victorwltsang <victorwltsang@gmail.com>
2019-02-26 00:25:48 -0800

    first commit

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	extends: 'stylelint-config-standard',
+	plugins: ['stylelint-scss'],
+	rules: {
+		'at-rule-no-unknown': null,
+		'scss/at-rule-no-unknown': true,
+		'no-descending-specificity': null,
+		indentation: [
+			'tab',
+			{
+				except: ['value']
+			}
+		]
+	}
+};
