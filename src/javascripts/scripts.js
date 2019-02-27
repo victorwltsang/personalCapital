253077cd8e5b8f7ca82563cd9b1318740a052bdd
Victor Tsang <vtsang@netbase.com>
2019-02-27 00:12:14 -0800

    finished header and nav

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,20 @@
 // Add your scripts here
+$(document).ready(() => {
+  $('.nav__hamburger').click(function () {
+    if ($(window).width() > 768) {
+      $('.tablet--show').toggle();
+    }
+
+    $(this).toggleClass('open');
+    $('.nav__list--mobile').toggleClass('open');
+  });
+
+  $(window).on('resize', () => {
+    if ($(window).width() > 1024) {
+      if ($('.nav__list--mobile').hasClass('open')) {
+        $('.nav__list--mobile').toggleClass('open');
+        $('.nav__hamburger').toggleClass('open');
+      }
+    }
+  });
+});
