1b09374d52bf408b1b4a33ef65bb01529ef55cbc
Victor Tsang <vtsang@netbase.com>
2019-02-27 16:21:55 -0800

    started carousel

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,15 +1,17 @@
 // Add your scripts here
 $(document).ready(() => {
   $('.nav__hamburger').click(function () {
-    if ($(window).width() > 768) {
-      $('.tablet--show').toggle();
-    }
-
     $(this).toggleClass('open');
     $('.nav__list--mobile').toggleClass('open');
+    if ($(window).width() > 768) {
+      $('.tablet--show ').toggle();
+    }
   });
 
   $(window).on('resize', () => {
+    if ($(window).width() > 1024 && $('.nav__list--mobile').hasClass('open')) {
+      $('.tablet--show').toggle();
+    }
     if ($(window).width() > 1024) {
       if ($('.nav__list--mobile').hasClass('open')) {
         $('.nav__list--mobile').toggleClass('open');
